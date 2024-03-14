4706d2b422aa73f09a0e9b39d62862d6c0808208
Cho Chan Woo <ccw040607@gmail.com>
2024-03-14 12:16:38 +0900

    changed to get .csv file through popup

--- popup.js
+++ popup.js
@@ -1,5 +1,8 @@
 const essential = document.getElementById('essential')
 const checkbox = document.getElementById('check_show')
+const csv_button = document.getElementById('getcsv')
+const save_button = document.getElementById('save')
+const clear_button = document.getElementById('clear')
 
 chrome.storage.session.get('essential')
     .then((response) => {
@@ -38,3 +41,60 @@ checkbox.onchange = () => {
     chrome.storage.session.set({'show' : checkbox.checked})
     sendMessage_to_currentTab("[ALOHA Rank List] show")
 }
+
+clear_button.onclick = () => {
+    if (confirm("Would you delete ALL data?"))
+        chrome.storage.local.clear().then(() => alert('Cleared'))
+}
+
+save_button.onclick = async () => {
+    csv_file = await csv_button.files[0].text()
+    if (csv_file.length === 0) alert('You should select .csv file to submit')
+    else if (confirm('Would you update to new data? Existing data will be deleted.')){
+        await csv_parse(csv_file)
+        alert(`${csv_button.files[0].name} Uploaded. Refresh page to apply`)
+    }
+}
+
+async function csv_parse(txt){
+    chrome.storage.local.clear()
+    const res = {}
+    let [h, ...data] = txt.split('\n')
+    head = h.trim().split(',')
+    // const encoder = new TextEncoder('euc-kr')
+    for (b of data){
+        now = b.split(',')
+        obj = {}
+        for (idx = 0; idx < head.length && idx < now.length; idx++){
+            const column_name = head[idx].trim().toLowerCase()
+            if (column_name === 'boj handle')
+                obj.handle = now[idx].toLowerCase()
+            else if (column_name === 'name')
+                obj.name = now[idx]
+            else if (column_name === 'division' && now[idx] !== '')
+                obj.division = now[idx]
+            else if (column_name === 'mentor' && now[idx] !== '' && !('division' in obj)){
+                obj.division = obj.mentor = now[idx]
+            }
+        }
+        if (obj.division){
+            if (obj.mentor) 
+                obj.division_priority = 10
+            else if (obj.division === '초급반')
+                obj.division_priority = 1
+            else if (obj.division === '중급반')
+                obj.division_priority = 2
+            else if (obj.division === '고급반')
+                obj.division_priority = 3
+            else
+                obj.division_priority = 9999
+        }
+        else
+            obj.division_priority = 9999
+        if (obj.handle)
+            res[obj.handle] = obj
+    }
+    chrome.storage.local.set({'handle_list' : res})
+}
+
+console.log(new Date().toLocaleTimeString())
